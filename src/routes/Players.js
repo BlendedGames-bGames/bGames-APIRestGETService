bc002dccbcc6a00fda467419791d1622b0735abd
Tisks <leiser.mahu@usach.com>
2020-12-13 17:49:27 -0300

    Adding 2 endpoints: get attributes by subattributes and get player's actual attributes levels

--- src/routes/Players.js
+++ src/routes/Players.js
@@ -21,6 +21,81 @@ router.get('/attributes/resume/:id',(req,res)=>{
 })
 
 /*
+Input: 
+"id_subattributes": Ej [5,2,1],   
+
+Output: Resume of attributes of that player
+Description: Simple MYSQL query
+*/
+router.get('/attributes_by_subattributes',(req,res)=>{
+    let id_subattributes = req.body.id_subattributes;
+
+    let select = 'SELECT`attributes`.`id_attributes`'
+    let from = 'FROM `attributes`'
+    let join = 'JOIN `subattributes` ON `attributes`.`id_attributes` = `subattributes`.`attributes_id_attributes`'
+
+    var thisaux = "";
+    for (let index = 0; index < id_subattributes.length-1; index++) {
+        thisaux += id_subattributes[index]+",";
+    }
+    thisaux += id_subattributes[id_subattributes.length-1]
+
+    let where = 'WHERE `subattributes`.`id_subattributes` IN ('+thisaux+')'
+    let orderBy = 'ORDER BY `attributes`.`id_attributes`  ASC'
+
+    let query = select+from+join+where+orderBy
+    mysqlConnection.query(query,(err,rows,fields)=>{
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+    console.log(id);
+})
+/*
+Input: 
+let player_attributes = {
+        "id_player":new_attribute_experience.id_player, //EJ: 1
+        "id_attributes":new_attribute_experience.id_attributes// Ej: [1,2]
+    }
+
+Output:
+
+data = [20,10]
+Description: Simple MYSQL query
+*/
+router.get('/player_attributes',(req,res)=>{
+    let id_player = req.body.id_player;
+    let id_attributes = req.body.id_attributes;
+
+    let select = 'SELECT `playerss_attributes`.`data`'
+    let from = 'FROM `playerss_attributes`'
+
+    var thisaux = "";
+    for (let index = 0; index < id_attributes.length-1; index++) {
+        thisaux += id_attributes[index]+",";
+    }
+    thisaux += id_attributes[id_attributes.length-1]
+
+    let where = 'WHERE `playerss_attributes`.`id_players` = ?  '
+    let and = 'AND `playerss_attributes`.`id_attributes` IN ('+thisaux+')'
+
+    let query = select+from+where+and
+    mysqlConnection.query(query,[id_player],(err,rows,fields)=>{
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+    console.log(id);
+})
+
+
+
+
+/*
 Input: Id of a player (range 0 to positive int)
 Output: Specific resume of a single attribute of that player
 Description: Simple MYSQL query
@@ -55,7 +130,7 @@ router.get('/attributes/:id',(req,res)=>{
                 thisaux += rows[index].id_attributes+",";
             }
             thisaux += rows[rows.length-1].id_attributes;
-            thisaux = 'SELECT*FROM `subattributes` where attributes_id_attributes IN('+thisaux+')'
+            thisaux = 'SELECT*FROM `subattributes` where attributes_id_attributes IN ('+thisaux+')'
             console.log("Esta Cosa tiene: "+ thisaux);
             mysqlConnection.query(thisaux,[], function(err2,rows2,fields2){
                 if (!err2){
