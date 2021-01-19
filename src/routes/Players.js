595a50ca97fb7917ad429d9152d92815a09849db
Tisks <leiser.mahu@usach.cl>
2021-01-19 19:09:13 -0300

    Example route

--- src/routes/Players.js
+++ src/routes/Players.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 
 
+router.get("/", (req,res) =>{
+    var variable = req.body
+    res.status(200).json(variable)
 
+});
 
 
 
