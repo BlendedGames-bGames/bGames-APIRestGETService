89396ba5f81d92ffd19e3cba0104fb8679ec7a6d
Tisks <leiser.mahu@usach.cl>
2021-01-20 23:11:50 -0300

    Testing watchtower CD with push example

--- src/routes/Players.js
+++ src/routes/Players.js
@@ -6,7 +6,7 @@ const mysqlConnection = require('../database');
 
 router.get("/", (req,res) =>{
     var variable = req.body
-    res.status(200).json(variable)
+    res.status(200).json("holas")
 
 });
 
