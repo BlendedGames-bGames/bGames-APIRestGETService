4e4781b18aaf503d7741b9053d16bfbf7e67a12c
Tisks <leiser.mahu@usach.cl>
2021-01-24 23:19:51 -0300

    Simple Usage (Enable All CORS Requests)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors'
 //Routes
 import attributes from './routes/attributes';
 
@@ -12,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
 
 //Routes
 app.use(attributes);
